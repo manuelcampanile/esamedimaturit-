0bce43e060bbdf8f4f7c6850ffc310784149a792
manuelcampanile <manuelcampanile45@gmail.com>
2026-06-24 07:47:07 +0000

    fase finale

---
+++ script.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const revealObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.18
+});
+
+reveals.forEach(el => revealObserver.observe(el));
